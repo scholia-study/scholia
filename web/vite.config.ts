6aaf93d7fe7565d339789b4f1ab731e2ff98c6e2
Filip Niklas <awdeegrays@gmail.com>
2026-03-08 17:36:01 +0100

    chore: add devtools

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,32 +1,48 @@
 import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
     plugins: [
-        nitro({
-            devProxy: {
-                "/api": {
-                    target: "http://localhost:4000",
-                    changeOrigin: true,
-                },
-            },
-            preset: "static", // This ensures it outputs files for Nginx, not a Node server
-            prerender: {
-                crawlLinks: true, // Automatically finds links and generates HTML for them
-                routes: ["/"], // Start crawling from the homepage
-            },
-            rollupConfig: { external: [/^@sentry\//] },
-        }),
+        devtools(),
+        // nitro({
+        //     // devProxy: {
+        //     //     "/api": {
+        //     //         target: "http://localhost:4000",
+        //     //         changeOrigin: true,
+        //     //     },
+        //     // },
+        //     // preset: "static", // This ensures it outputs files for Nginx, not a Node server
+        //     // prerender: {
+        //     //     crawlLinks: true, // Automatically finds links and generates HTML for them
+        //     //     routes: ["/"], // Start crawling from the homepage
+        //     // },
+        //     // rollupConfig: { external: [/^@sentry\//] },
+        // }),
         tsconfigPaths({ projects: ["./tsconfig.json"] }),
         tailwindcss(),
         // 2. You can also enable prerendering inside the tanstackStart plugin
         tanstackStart({
             prerender: {
                 enabled: true,
+                crawlLinks: true,
+                autoStaticPathsDiscovery: true,
+                onSuccess: ({ page }) => {
+                    console.info(`🟡 Rendered ${page.path}`);
+                },
+            },
+            spa: {
+                enabled: true,
+                prerender: {
+                    enabled: true,
+                    crawlLinks: true,
+                    onSuccess: ({ page }) => {
+                        console.info(`Rendered ${page.path}!`);
+                    },
+                },
             },
         }),
         viteReact(),
