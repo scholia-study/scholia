ac261d982e5e404054b85cade569738ffe4052c0
Filip Niklas <awdeegrays@gmail.com>
2026-03-09 08:24:37 +0100

    chore: cleanup config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -8,30 +8,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 const config = defineConfig({
     plugins: [
         devtools(),
-        // nitro({
-        //     // devProxy: {
-        //     //     "/api": {
-        //     //         target: "http://localhost:4000",
-        //     //         changeOrigin: true,
-        //     //     },
-        //     // },
-        //     // preset: "static", // This ensures it outputs files for Nginx, not a Node server
-        //     // prerender: {
-        //     //     crawlLinks: true, // Automatically finds links and generates HTML for them
-        //     //     routes: ["/"], // Start crawling from the homepage
-        //     // },
-        //     // rollupConfig: { external: [/^@sentry\//] },
-        // }),
         tsconfigPaths({ projects: ["./tsconfig.json"] }),
         tailwindcss(),
-        // 2. You can also enable prerendering inside the tanstackStart plugin
         tanstackStart({
             prerender: {
                 enabled: true,
                 crawlLinks: true,
                 autoStaticPathsDiscovery: true,
                 onSuccess: ({ page }) => {
-                    console.info(`🟡 Rendered ${page.path}`);
+                    console.info(`🧼 Rendered ${page.path}`);
                 },
             },
             spa: {
@@ -40,18 +25,13 @@ const config = defineConfig({
                     enabled: true,
                     crawlLinks: true,
                     onSuccess: ({ page }) => {
-                        console.info(`Rendered ${page.path}!`);
+                        console.info(`🖼️ Rendered ${page.path}!`);
                     },
                 },
             },
         }),
         viteReact(),
     ],
-    server: {
-        proxy: {
-            "/api": "http://localhost:4000",
-        },
-    },
     clearScreen: false,
 });
 
